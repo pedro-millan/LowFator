378dd501ab5d54e80dcfbcaa98ecb00485e2bbcc
Pedro Millán <millanpedrop@gmail.com>
2025-11-21 19:01:31 +0100

    chore: 8 filter buttons and footer added;

--- lowfator-frontend/src/App.js
+++ lowfator-frontend/src/App.js
@@ -59,6 +59,50 @@ function App() {
                       />
                       <h1 className='add'>ADD TEXTURES AND DIRT YOUR SOUNDS</h1>
                     </div>
+      
+                    <div className="contenedor">
+                      <div className="item">
+                        <img src="/lofi.webp" alt="Lo-Fi filter" />
+                      </div>
+                      <div className="item">
+                        <img src="/8bit.webp" alt="8 Bit converter" />
+                      </div>
+                      <div className="item" id='bigger'>
+                        <img src="/tapedistortion.webp" alt="Tape Distortion filter" />
+                      </div>
+                      <div className="item" id='bigger'>
+                        <img src="/compressor.webp" alt="Compressor" />
+                      </div>
+                      <div className="item">
+                        <img src="/vinylcrackle.webp" alt="Vinyl Crackle filter" />
+                      </div>
+                      <div className="item">
+                        <img src="/dirtyreverb.webp" alt="Dirty Reverb filter" />
+                      </div>
+                      <div className="item" id='bigger'>
+                        <img src="/woobler.webp" alt="Wooble filter" />
+                      </div>
+                      <div className="item" id='bigger'>
+                        <img src="/glitchdelay.webp" alt="Glitch Delay filter" />
+                      </div>
+                    </div>
+
+                    <div className="footer">
+                    <img
+                      src="/intro3.gif"
+                      alt="LowFator Logo Animation"
+                      className="logo-gif2"
+                    />
+                      <div className="redes">
+                        <img src="/insta.webp" alt="Instagram logo" className='footer-logos' id='logo1'/>
+                        <img src="/gh.webp" alt="GitHub logo" className='footer-logos'/>
+                        <img src="/li.webp" alt="LinkedIn logo" className='footer-logos'/>
+                      </div>
+                      <a href="https://www.privacypolicies.com/live/95e7cf62-f373-4c69-8806-00aee7611b2e" id="footer_text" target="_blank" rel="noopener noreferrer">
+                        Política de Privacidad
+                      </a>
+                      <p id="footer_text2">© 2025 Pedro P. Millán Mompó ・ All Rights Reserved. </p>
+                    </div>
                   </div>
                 )}
               </>
